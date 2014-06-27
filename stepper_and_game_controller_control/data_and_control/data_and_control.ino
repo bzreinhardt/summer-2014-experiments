5cda3dfdb0eef364eadcbe398f3e43b6ec14c8d3
Ben Reinhardt <bzreinhardt@gmail.com>
2014-06-27 09:34:10 -0700

    stable experiment code with working controller, LED, motors, data recording

--- stepper_and_game_controller_control/data_and_control/data_and_control.ino
+++ stepper_and_game_controller_control/data_and_control/data_and_control.ino
@@ -6,27 +6,36 @@
 Modified by Benjamin Reinhardt (bzreinhardt on github and gmail to control a stepper motor based on ps3 controller)
  */
 
+ #include <SD.h>
+
+// On the Ethernet Shield, CS is pin 4. Note that even if it's not
+// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
+// 53 on the Mega) must be left as an output or the SD library
+// functions will not work.
+//
+
+
 #include <PS3USB.h>
-#include <SD.h>
 // Satisfy IDE, which only needs to see the include statment in the ino.
 #ifdef dobogusinclude
 #include <spi4teensy3.h>
 #endif
-#include <AccelStepper.h>
 
-// Define two steppers and the pins they will use
-AccelStepper stepper1(1, 3, 2);
-AccelStepper stepper2(1, 5, 4);
+#include <AccelStepper.h>
 
 USB Usb;
 /* You can create the instance of the class in two ways */
 PS3USB PS3(&Usb); // This will just create the instance
 //PS3USB PS3(&Usb,0x00,0x15,0x83,0x3D,0x0A,0x57); // This will also store the bluetooth address - this can be obtained from the dongle when running the sketch
 
+// Chip Select pin is tied to pin 8 on the SparkFun SD Card Shield
+const int chipSelect = 8;  
+
 boolean printAngle;
 uint8_t state = 0;
 //set the direction and enable pins for the motor controller
-int dirpin1 = 0;
+/*
+int dirpin1 = 0; 
 int steppin1 = 1;
 int dirpin2 = 2;
 int steppin2 = 3;
@@ -34,27 +43,56 @@ int dirpin3 = 4;
 int steppin3 = 5;
 int dirpin4 = 6;
 int steppin4 = 7;
-int i = 0;
+*/
+// variables keeping track of the controller commands
+int LX = 0;
+int LY = 0;
+int RX = 0;
+int RY = 0;
+int oldtime = 0;
+int newtime = 0;
 
+AccelStepper stepper1(1, 3, 2);
+AccelStepper stepper2(1, 5, 4);
+AccelStepper stepper3(1, 7, 6);
 
-int pos1 = 3600;
-int pos2 = 5678;
-// variable turn data acquisition on and off
-boolean dataAcq = false; 
+// keep track of positions of stepper motors
+int pos1 = 0;
+int pos2 = 0;
+int pos3 = 0;
+int d1 = 0;
+int d2 = 0;
+float speed = 4000;
+
 boolean direction = true; 
-// Chip Select pin is tied to pin 8 on the SparkFun SD Card Shield
-const int chipSelect = 8;  
 
 void setup() {
+  pinMode(chipSelect, OUTPUT);
+  pinMode(1, OUTPUT);
   
-  Serial.begin(9600);
-  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+  //Serial.begin(9600);
+  //while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
   if (Usb.Init() == -1) {
-    Serial.print(F("\r\nOSC did not start"));
-    while (1); //halt
+    //Serial.print(F("\r\nOSC did not start"));
+    //while (1); //halt
   }
+
+  // see if the card is present and can be initialized:
+  
+  if (!SD.begin(chipSelect)) {
+    //Serial.println("\r\nCard failed, or not present");
+    // don't do anything more:
+    //return;
+  }
+  else{
+   //Serial.println("\r\ncard initialized.");
+ }
+ 
+
+
   
   //Set the control pins as outputs
+  /*
   pinMode(dirpin1, OUTPUT);
   pinMode(steppin1, OUTPUT);
   pinMode(dirpin2, OUTPUT);
@@ -63,188 +101,119 @@ void setup() {
   pinMode(steppin3, OUTPUT);
   pinMode(dirpin4, OUTPUT);
   pinMode(steppin4, OUTPUT);
+  */
 
-  stepper1.setMaxSpeed(3000);
+  stepper1.setMaxSpeed(7000);
   stepper1.setAcceleration(10000);
-  stepper2.setMaxSpeed(3000);
-  stepper2.setAcceleration(10000);
   stepper1.setSpeed(0);
+  stepper1.setMinPulseWidth(20);
+
+  stepper2.setMaxSpeed(7000);
+  stepper2.setAcceleration(10000);
   stepper2.setSpeed(0);
+  stepper2.setMinPulseWidth(20);
 
-  // make sure that the default chip select pin is set to
-  // output, even if you don't use it:
-  Serial.print("Initializing SD card...");
-  // make sure that the default chip select pin is set to
-  // output, even if you don't use it:
-  pinMode(chipSelect, OUTPUT);
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return;
-  }
-  Serial.println("card initialized.");
+  stepper3.setMaxSpeed(7000);
+  stepper3.setAcceleration(10000);
+  stepper3.setSpeed(0);
+  stepper3.setMinPulseWidth(20);
 
   
   //print that you've started
-  Serial.print("\r\nTest started");
+  //Serial.print(F("\r\nPS3 USB Library Started"));
 
 }
 
 
-void loop() 
-{
+void loop() {
+  // check the controller values
   Usb.Task();
-  stepper1.runSpeed();
-  stepper2.runSpeed();
-  i++;
-  if(i%20 == 0){
   if (PS3.PS3Connected || PS3.PS3NavigationConnected) {
-    if (PS3.getButtonClick(CROSS)){
-      dataAcq = !dataAcq;
-    }
-    int left_x = PS3.getAnalogHat(LeftHatX);
-    int left_y = PS3.getAnalogHat(LeftHatY);
-    int right_x = PS3.getAnalogHat(RightHatX);
-    int right_y = PS3.getAnalogHat(RightHatX);
-    if (dataAcq == true){
-      File dataFile = SD.open("datalog2.txt", FILE_WRITE);
-      if (dataFile) 
-      {  
+    if (PS3.getButtonClick(UP)) {
+      digitalWrite(1, HIGH);
+      Serial.print("U");
+      if (d2 < 0){d2 = 0;}
+      else if (d2 == 0){d2 = 1;}
+      else if (d2 > 0){d2 = 1;}
+      stepper2.setSpeed(d2*speed);
+      stepper3.setSpeed(d2*speed);
+      
+      recordData(millis(), stepper1.speed(), stepper2.speed());
+      File dataFile = SD.open("data.txt", FILE_WRITE);
+      if(dataFile){
         int timeStamp = millis();
+        float s1 = stepper1.speed();
+        float s23 = stepper2.speed();
         dataFile.print(timeStamp);
         dataFile.print(", ");
-        Serial.print(timeStamp);
-        Serial.print(", ");
-
-      // read three sensors and append to the string:
-        dataFile.print(left_x);
+        dataFile.print(s1);
         dataFile.print(", ");
-        dataFile.print(left_y);
-        dataFile.print(", ");
-        dataFile.print(right_x);
-        dataFile.print(", ");
-        dataFile.print(right_y);
-        dataFile.println();
+        dataFile.print(s23);
+        dataFile.print("\n");
         dataFile.close();
-        /*
-        Serial.print(left_x);
-        Serial.print(", ");
-        Serial.print(left_y);
-        Serial.print(", ");
-        Serial.print(right_x);
-        Serial.print(", ");
-        Serial.print(right_y);
-        */
-
-      // print to the serial port too:
-      }  
-    // if the file isn't open, pop up an error:
-      else
-      {
-        Serial.println("error opening datalog.txt");
       }
-    } 
-    //Serial.print(" stepper1 ");
-    driveMotor(stepper1, (float)left_x);
-    //Serial.print(" stepper2 ");
-    driveMotor(stepper2, (float)right_x);
-  }
+      
+
+    }
+    if (PS3.getButtonClick(DOWN)){
+      digitalWrite(1, LOW);
+      if (d2 < 0){d2 = -1;}
+      else if (d2 == 0){d2 = -1;}
+      else if (d2 > 0){d2 = 0;}
+      stepper2.setSpeed(d2*speed);
+      stepper3.setSpeed(d2*speed);
+      //recordData(millis(), stepper1.speed(), stepper2.speed());
+    }
+
+    if (PS3.getButtonClick(RIGHT)) {
+      if (d1 < 0){d1 = 0;}
+      else if (d1 == 0){d1 = 1;}
+      else if (d1 > 0){d1 = 1;}
+      stepper1.setSpeed(d1*speed);
+      //recordData(millis(), stepper1.speed(), stepper2.speed());
+      
+      
+    }
     
-    
+    if (PS3.getButtonClick(LEFT)) {
+      if (d1 < 0){d1 = -1;}
+      else if (d1 == 0){d1 = -1;}
+      else if (d1 > 0){d1 = 0;}
+      stepper1.setSpeed(d1*speed);
+      //recordData(millis(), stepper1.speed(), stepper2.speed());
+    }
+
+    //Serial.print(" LX = ");
+    //Serial.print(LX);
+    //Serial.print(" RX = ");
+    //Serial.print(RX);
     //Serial.println();
-    
-/*
-    if (PS3.getAnalogHat(LeftHatX) > 137 || PS3.getAnalogHat(LeftHatX) < 117 ) 
-    {
-      //Serial.print(F("\r\nLeftHatX: "));
-      //Serial.print(PS3.getAnalogHat(LeftHatX));
-      driveMotor(dirpin1, steppin1, left_x);
-    }
-
-    if (PS3.getAnalogHat(LeftHatY) > 137 || PS3.getAnalogHat(LeftHatY) < 117 )
-    {
-      //Serial.print(F("\r\nLeftHatY: "));
-      //Serial.print(PS3.getAnalogHat(LeftHatY));
-      driveMotor(dirpin2, steppin2, left_y);
-    }
-    if (PS3.getAnalogHat(RightHatX) > 137 || PS3.getAnalogHat(RightHatX) < 117 )
-    {
-      //Serial.print(F("\r\nRightHatX: "));
-      //Serial.print(PS3.getAnalogHat(RightHatX));
-      driveMotor(dirpin3, steppin3, right_x);
-    }
-    if (PS3.getAnalogHat(RightHatY) > 137 || PS3.getAnalogHat(RightHatY) < 117)
-    {
-      //Serial.print(F("\r\nRightHatY: "));
-      //Serial.print(PS3.getAnalogHat(RightHatY));
-       driveMotor(dirpin4, steppin4, right_y);
-    }
-      // analog values between 0-255. 0 - 117 direction 1. 137 - 255 direction 2.
-    */
   }
   
+  // find delta in position based on inputs
+  /*
+  if (PS3.getButtonClick()){
+    d1 = 10*(LX - 127);
+    pos1 = stepper1.currentPosition();
+    pos3 = stepper3.currentPosition();
+    stepper1.moveTo(pos1 + d1);
+    stepper3.moveTo(pos3 + d1);
+  }
+  if (RX < 117 || RX > 137){
+    d2 = 20*(RX - 127);
+    pos2 = stepper2.currentPosition();
+    stepper2.moveTo(pos2 + d2);
+  }
+  // set the new positions for each motor
+  // run the motors towards the new position
+  */
+stepper1.runSpeed();
+stepper2.runSpeed();
+stepper3.runSpeed();
+
 }
 
-void driveMotor(AccelStepper motor, float speed){
-// analog values between 0-255. 0 - 117 direction 1. 137 - 255 direction 2.
-
-
-  if (speed < 117){
-    float motorSpeed = -1*1000*(117-speed)/117;
-    Serial.print(motorSpeed);
-    motor.setSpeed(motorSpeed);
-    
-    
-  }
-  else if (speed > 137){
-    float motorSpeed = 1000*(speed - 137)/118;
-    Serial.print(motorSpeed);
-
-    motor.setSpeed(motorSpeed);
-    
-  }
-  else {
-    Serial.print(0);
-    motor.setSpeed(0);
-    
-  }
-  
+void recordData(float time, float s1, float s23){
+      
 }
 
-  /* Rumble things - not necessary
-  else if (PS3.PS3MoveConnected) { // One can only set the color of the bulb, set the rumble, set and get the bluetooth address and calibrate the magnetometer via USB
-    if (state == 0) {
-      PS3.moveSetRumble(0);
-      PS3.moveSetBulb(Off);
-    } else if (state == 1) {
-      PS3.moveSetRumble(75);
-      PS3.moveSetBulb(Red);
-    } else if (state == 2) {
-      PS3.moveSetRumble(125);
-      PS3.moveSetBulb(Green);
-    } else if (state == 3) {
-      PS3.moveSetRumble(150);
-      PS3.moveSetBulb(Blue);
-    } else if (state == 4) {
-      PS3.moveSetRumble(175);
-      PS3.moveSetBulb(Yellow);
-    } else if (state == 5) {
-      PS3.moveSetRumble(200);
-      PS3.moveSetBulb(Lightblue);
-    } else if (state == 6) {
-      PS3.moveSetRumble(225);
-      PS3.moveSetBulb(Purble);
-    } else if (state == 7) {
-      PS3.moveSetRumble(250);
-      PS3.moveSetBulb(White);
-    }
-
-    state++;
-    if (state > 7)
-      state = 0;
-    delay(1000);
-  }
-}
-*/
